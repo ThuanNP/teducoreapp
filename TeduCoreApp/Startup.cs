59394b570a259d7e4bee8bf3c410d012452d03d9
ThuanNP <nguyenphuocthuan1803@gmail.com>
2018-12-06 16:23:52 +0700

    Creating admin area

--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -96,6 +96,10 @@ namespace TeduCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                     name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
 
             dbIntinitializer.Seed().Wait();
