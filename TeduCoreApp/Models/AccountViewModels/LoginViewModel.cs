c9699a149a8fe01b7e1b7376cc8010e57f7ea825
ThuanNP <nguyenphuocthuan1803@gmail.com>
2019-02-28 10:31:40 +0700

    Finish client login feature.

--- TeduCoreApp/Models/AccountViewModels/LoginViewModel.cs
+++ TeduCoreApp/Models/AccountViewModels/LoginViewModel.cs
@@ -13,13 +13,13 @@ namespace TeduCoreApp.Models.AccountViewModels
         //public string Email { get; set; }
 
         [Required]
-        public string Username { get; set; }
+        public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Remember me")]
         public bool RememberMe { get; set; }
     }
 }
